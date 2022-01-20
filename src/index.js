04c7511f68b4dd2058117f834b73cece3c0da93e
Jorge Pérez <80980397+CodenautaJorge@users.noreply.github.com>
2022-01-20 12:53:16 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import fetch from 'node-fetch';
+import utm from 'utm';
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+//Lanzamos el servidor
+app.listen(app.get('port'), () =>{
+    console.log("Servidor en el puerto ", app.get('port'));
+});
+
+
+const url = 'http://mapas.valencia.es/lanzadera/opendata/Valenbisi/JSON';
+const response = await fetch(url);
+const data = await response.json();
+//console.log(data);
+
+const valenbisi = [];
+
+
+for(let i = 0; i < data.features.length; i++){
+
+    valenbisi.push({
+        'address' : data.features[i].properties.address, 
+        'free': data.features[i].properties.free,
+        'coords': utm.toLatLon(data.features[i].geometry.coordinates[0], data.features[i].geometry.coordinates[1], 30, "N")
+    });
+}
+
+console.log(valenbisi);
+
+app.get('/valenbisi', (req, res) => {    
+    res.json(
+        {
+            "valenbisi": valenbisi
+        }
+    );
+});
+
